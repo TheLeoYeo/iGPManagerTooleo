1c93664f11ffd133cacf28b2611ccdc161daba81
TheLeoYeo <leo.yeofr@gmail.com>
2022-11-01 20:14:00 +0000

    setup commands added to igp account

--- tool/python/igp/service/igpaccount.py
+++ tool/python/igp/service/igpaccount.py
@@ -1,6 +1,7 @@
 from igp.service.commands.car_repair import CarRepairCommands
 from igp.service.commands.driver_train import DriverTrainCommands
+from igp.service.commands.set_strategy import SetupCommands
 
 
-class IGPaccount(CarRepairCommands, DriverTrainCommands):
+class IGPaccount(CarRepairCommands, DriverTrainCommands, SetupCommands):
     pass
