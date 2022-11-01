dec7048028d45ffde9d295bbeaf1ed14ca63b58f
TheLeoYeo <leo.yeofr@gmail.com>
2022-11-01 20:10:47 +0000

    Modifier + help message
    Commands now have more options for help message including just the command's alias, a given help message or the docstring of the function of the command. Yippee
    
    BadModifier exception is thrown if I do something silly with a modifier

--- tool/python/igp/util/decorators.py
+++ tool/python/igp/util/decorators.py
@@ -4,17 +4,27 @@ from typing import Callable
 
 from igp.service.base_igp_account import BaseIGPaccount
 from igp.util.tools import output
+from igp.service.modifier.modifier import BaseModifier
+from igp.util.exceptions import BadModifierException
 
 
 class Command():
-    def __init__(self, alias:str, function:Callable, param_names:dict):
+    def __init__(self, alias:str, function:Callable, help:str=None, modifier:BaseModifier=None):
         self.alias = alias
         self.function = function
-        self.params:dict = param_names
+        self.help_text = alias
+        if help:
+            self.help_text = help
+            
+        if modifier:
+            self.modifier = modifier
+        else:
+            self.modifier = BaseModifier()
        
         
     def perform(self, account:BaseIGPaccount):
-        self.function(account, **self.params)
+        params = self.modifier.params()
+        self.function(account, **params)
 
    
     def __str__(self):
@@ -22,15 +32,18 @@ class Command():
     
     
     def help(self):
-        return self.__str__()
+        return self.help_text
 
 
-def igpcommand(_func:Callable=None, *, page:str=None, alias:str=None):
+def igpcommand(_func:Callable=None, *, page:str=None, alias:str=None, help:str=None, modifier:BaseModifier=None):
     def decorator_command(func:Callable):
         _alias = alias
         if not _alias:
             _alias = func.__name__
-            
+        
+        _help = help
+        if not _help:
+            _help = func.__doc__
             
         @functools.wraps(func)
         def wrapper(self:BaseIGPaccount, *args, **kwargs):
@@ -54,13 +67,19 @@ def igpcommand(_func:Callable=None, *, page:str=None, alias:str=None):
             output(f"Finished doing task '{_alias}'", log_only=True)
         
         
-        argspec = inspect.getfullargspec(func)
-        param_names = dict.fromkeys(argspec[0][1:])
-        values = argspec[3]
-        for index, param in enumerate(param_names.keys()):
-            param_names[param] = values[index]
+        if modifier:
+            argspec = inspect.getfullargspec(func)
+            # make sure modifier includes all parameters
+            req_params = argspec[0][1:]
+            mod_params = modifier.params()
+            if len(req_params) != len(mod_params):
+                raise BadModifierException(f"Wrong number of parameters found in modifier ")
+            
+            for req_param in req_params:
+                if req_param not in mod_params:
+                    raise BadModifierException(f"Parameter '{req_param}' was not found in modifier")
         
-        BaseIGPaccount.commands.append(Command(_alias, wrapper, param_names))
+        BaseIGPaccount.commands.append(Command(_alias, wrapper, _help, modifier))
         return wrapper
     
     if not _func:
