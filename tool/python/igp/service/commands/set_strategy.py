eb18deb9cb1e051ca47d8c25868eeaffb68ae80a
LEVI YEO <k21009934@kcl.ac.uk>
2022-11-11 18:10:55 +0000

    save functionality + bug fix
    Now the thing actually saves the setup once it is done setting it.
    
    Also a bug with changing setup was fixed.

--- tool/python/igp/service/commands/set_strategy.py
+++ tool/python/igp/service/commands/set_strategy.py
@@ -18,38 +18,37 @@ class Suspension(Enum):
     SOFT = 0
     NEUTRAL = 1
     FIRM = 2
+    
+    
+class DriverTab(Enum):
+    DRIVER1 = 1
+    DRIVER2 = 2
 
 
 class SetupCommands(BaseIGPaccount):
     strategy_page = "httpshttps://igpmanager.com/app/p=race&tab=strategy"
     setup_page = "https://igpmanager.com/app/p=race&tab=setup"
     ALL_SUSP_TYPES = [Suspension.SOFT, Suspension.NEUTRAL, Suspension.FIRM]
+    ALL_DRV_TYPES = [DriverTab.DRIVER1, DriverTab.DRIVER2]
     
     @igpcommand(alias="setup all drivers", page=setup_page, help="Sets the setup of all drivers in the account", 
                 modifier=BaseModifier(OptionField("suspension", ALL_SUSP_TYPES, 0), IntegerField("rheight", 1, 50, 20), IntegerField("wlevel", 1, 50, 20)))
     def setup_drivers(self, suspension:Suspension=Suspension.SOFT, rheight:int=20, wlevel:int=20):
-        try:
-            for i in range(1):
-                self.setup_driver(1 + i, suspension, rheight, wlevel)
-        except NoSuchPilotError:
-            pass
+        for driver in self.ALL_DRV_TYPES:
+            self.setup_driver(driver, suspension, rheight, wlevel)
 
 
     @igpcommand(alias="setup a driver", page=setup_page, 
-                modifier=BaseModifier(IntegerField("driver", 1, 2), OptionField("suspension", ALL_SUSP_TYPES, 0), IntegerField("rheight", 1, 50, 20), IntegerField("wlevel", 1, 50, 20)))
-    def setup_driver(self, driver:int=1, suspension:Suspension=Suspension.SOFT, rheight:int=20, wlevel:int=20):
-        """Set setup for a driver
-        1 for driver 1 and 2 for driver 2"""
-        
-        if driver > 2:
-            raise NoSuchPilotError()
+                modifier=BaseModifier(OptionField("driver", ALL_DRV_TYPES), OptionField("suspension", ALL_SUSP_TYPES, 0), IntegerField("rheight", 1, 50, 20), IntegerField("wlevel", 1, 50, 20)))
+    def setup_driver(self, driver:DriverTab=DriverTab.DRIVER1, suspension:Suspension=Suspension.SOFT, rheight:int=20, wlevel:int=20):
+        """Set setup for a driver"""
 
         try:
-            WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.ID, f"driver{driver}")))
-        except TimeoutException:
-            raise NoSuchPilotError()
+            self.grab_tab(driver)
+        except NoSuchPilotError:
+            return
         
-        form = self.driver.find_element(By.ID, f"d{driver}setup")
+        form = self.driver.find_element(By.ID, f"d{driver.value}setup")
         table = form.find_element(By.CLASS_NAME, "linkFill")
         rows = table.find_elements(By.TAG_NAME, "tr")
 
@@ -59,6 +58,17 @@ class SetupCommands(BaseIGPaccount):
         output(f"ride height set to {rh}", log_only=True)
         wl = self.set_row_value(rows[2], wlevel)
         output(f"wing level set to {wl}", log_only=True)
+        self.hit_save()
+        
+        
+    def grab_tab(self, tab:DriverTab):
+        num = tab.value
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.ID, f"driver{num}")))
+        button = self.driver.find_element(By.ID, f"driver{num}")
+        if "disabled" in button.get_attribute("class"):
+            raise NoSuchPilotError()
+
+        click(button)
 
 
     def set_suspension(self, row:WebElement, suspension:Suspension):
@@ -83,8 +93,7 @@ class SetupCommands(BaseIGPaccount):
         action = ActionChains(self.driver)
         action.click_and_hold(decrement).perform()
         while "disabled" not in decrement.get_attribute("class") and value < int(current_box.text):
-            pass
-        
+            pass       
         action.release(decrement).perform()
         
         current = int(current_box.text)
@@ -93,9 +102,9 @@ class SetupCommands(BaseIGPaccount):
         
         action = ActionChains(self.driver)
         action.click_and_hold(increment).perform()
-        while "disabled" not in decrement.get_attribute("class") and value > int(current_box.text):
+        while "disabled" not in increment.get_attribute("class") and value > int(current_box.text):
             pass
-        action.release(decrement).perform()
+        action.release(increment).perform()
         
         current = int(current_box.text)
         if current == value:
