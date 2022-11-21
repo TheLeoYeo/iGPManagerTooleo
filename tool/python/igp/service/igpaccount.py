8e9629a1c4f731c9ffbcc5ed03c7053c9ca24b8b
LEVI YEO <leo.yeofr@gmail.com>
2022-11-21 23:25:10 +0000

    Update igpaccount.py

--- tool/python/igp/service/igpaccount.py
+++ tool/python/igp/service/igpaccount.py
@@ -1,7 +1,8 @@
 from igp.service.commands.car_repair import CarRepairCommands
 from igp.service.commands.driver_train import DriverTrainCommands
-from igp.service.commands.set_strategy import SetupCommands
+from igp.service.commands.set_setup import SetupCommands
+from igp.service.commands.set_strategy import StrategyCommands
 
 
-class IGPaccount(CarRepairCommands, DriverTrainCommands, SetupCommands):
+class IGPaccount(CarRepairCommands, DriverTrainCommands, SetupCommands, StrategyCommands):
     pass
