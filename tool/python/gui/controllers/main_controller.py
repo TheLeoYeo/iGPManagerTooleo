58984eca5ff3f3c67b6776fe73afd169a9be5363
LEVI YEO <leo.yeofr@gmail.com>
2022-11-21 23:27:25 +0000

    Turbo mode + logs button functionality

--- tool/python/gui/controllers/main_controller.py
+++ tool/python/gui/controllers/main_controller.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets, uic
 
 from gui.components.output_window import OutputWindow
 from gui.components.login_window import LoginWindow
+from gui.components.logs_window import LogsWindow
 from gui.gui_util.ready_containers import ReadyContainers
 from igp.service.accounts import AccountIterator
 from igp.service.jobs import AllJobs, Job
@@ -18,6 +19,7 @@ class Main(QtWidgets.QMainWindow, UI_Window):
         self.setupUi(self)
         self.setAcceptDrops(True)
         self.addlogin = LoginWindow(self)
+        self.logswindow = LogsWindow(self)
         self.output = OutputWindow(self)
         
         self.load = load
@@ -29,6 +31,8 @@ class Main(QtWidgets.QMainWindow, UI_Window):
         self.remove_account.pressed.connect(self.remove_acc)
         self.add_jobs_butt.pressed.connect(self.add_jobs)
         self.remove_jobs_butt.pressed.connect(self.remove_jobs)
+        self.turboModeButton.pressed.connect(self.toggle_turbo)
+        self.logsButton.pressed.connect(self.show_logs)
         self.perform_butt.pressed.connect(self.perform)
     
       
@@ -40,6 +44,10 @@ class Main(QtWidgets.QMainWindow, UI_Window):
     
     def create_login(self):
         self.addlogin.show()
+        
+    
+    def show_logs(self):
+        self.logswindow.show()
         
         
     def handle(self, event:Event):
@@ -58,9 +66,16 @@ class Main(QtWidgets.QMainWindow, UI_Window):
     def perform(self):
         self.add_jobs_butt.setEnabled(False)
         self.remove_jobs_butt.setEnabled(False)
-        self.jobsCont.perform([self.add_jobs_butt, self.remove_jobs_butt])
+        self.perform_butt.setEnabled(False)
+        self.jobsCont.perform([self.add_jobs_butt, self.remove_jobs_butt, self.perform_butt])
         
-           
+        
+    def toggle_turbo(self):
+        self.jobsCont.toggle_turbo()
+        tm = ("OFF", "ON")[self.jobsCont.turbo_enabled()]
+        self.turboModeButton.setText(f"Turbo: {tm}")
+    
+    
     def add_jobs(self):
         Job(self.accountsCont.selected.copy(), self.tasksCont.selected.copy())
        
