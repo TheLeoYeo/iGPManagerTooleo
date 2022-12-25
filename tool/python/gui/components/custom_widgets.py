4d52457d065863df2036fda8e86cd90501261002
Levi Yeo <leo.yeofr@gmail.com>
2022-12-25 06:14:53 +0000

    added left + right button
    These buttons use the fieldbutton class form field components

--- tool/python/gui/components/custom_widgets.py
+++ tool/python/gui/components/custom_widgets.py
@@ -1,7 +1,27 @@
 from PyQt5.QtWidgets import QLabel, QCheckBox
- 
+from PyQt5.QtCore import QSize
+
+from python.gui.components.field_component import ButtonType, FieldButton
+
+
 class Heading(QLabel):
     pass
 
 class Text(QCheckBox):
-    pass    
+    pass
+
+
+class LeftButton(FieldButton):
+    DEF_STYLES = "FieldButton{padding:1px;"
+    
+    def __init__(self, parent):
+        FieldButton.__init__(self, parent, type=ButtonType.LEFT)
+        self.setMaximumSize(QSize(20, 20))
+
+
+class RightButton(FieldButton):
+    DEF_STYLES = "FieldButton{padding:1px;"
+    
+    def __init__(self, parent):
+        FieldButton.__init__(self, parent, type=ButtonType.RIGHT)
+        self.setMaximumSize(QSize(20, 20))
